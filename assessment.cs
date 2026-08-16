9795ebcbd431f65866207e6159f1d4c27970e041
OMI786I <sohrawardy1998@gmail.com>
2026-08-16 23:12:06 +0600

    finished 1st assessment

---
+++ assessment.cs
@@ -0,0 +1,12 @@
+//1. Suppose decimal gradePointAverage = 3.99872831m;. What is the value of (int) gradePointAverage?
+
+
+var gradePointAverage = 3.99872831m;
+var value = (int) gradePointAverage;
+
+//Console.WriteLine(value);
+
+//2. In the following code decimal x = 7 / 5; why does x = 1?
+
+float x =7/5;
+Console.WriteLine(x);
